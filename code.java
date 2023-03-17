9e836965c082bba960d060a9233bc0736b67d143
kannan 20 <kannan20k@gmail.com>
2023-03-17 07:19:11 +0000

    3rd commit

--- code.java
+++ code.java
@@ -2,7 +2,7 @@
 public static void main(String[] String)   
 {  
 int ch1 = 'a';  
-int ch2 = 'c';  
+int ch2 = 'd';  
 System.out.println("The ASCII value of a is: "+ch1);  
 System.out.println("The ASCII value of b is: "+ch2);  
 }  
